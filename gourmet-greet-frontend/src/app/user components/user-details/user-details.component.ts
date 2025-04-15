07d1a0ec10861ea461cbc32f365fd427635f7c2c
Mila-Slavkovska <milaslavkovska@gmail.com>
2025-04-15 12:28:14 +0200

    Add styling for user details

--- gourmet-greet-frontend/src/app/user components/user-details/user-details.component.ts
+++ gourmet-greet-frontend/src/app/user components/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-details',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './user-details.component.css'
 })
 export class UserDetailsComponent {
-
+  //TODO: Find number of reviews for every user
+  @Input() user?: User
+  numReviews = 0
 }
--- gourmet-greet-frontend/src/app/user components/user-profile/user-profile.component.ts
+++ gourmet-greet-frontend/src/app/user components/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserDetailsComponent } from '../user-details/user-details.component';
 import { UserRecipesComponent } from '../user-recipes/user-recipes.component';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,5 +11,11 @@ import { UserRecipesComponent } from '../user-recipes/user-recipes.component';
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+  authService = inject(AuthService)
+  user?: User
 
+  ngOnInit(): void {
+    this.authService.setUser()
+    this.user = this.authService.getUser() || undefined
+  }
 }

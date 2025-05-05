0d5ec1afeda4f6ee20db15b3ea895cf91a013fda
Mila-Slavkovska <milaslavkovska@gmail.com>
2025-05-05 13:17:36 +0200

    Add form validation on FE

--- gourmet-greet-frontend/src/app/recipe-form/recipe-form.component.ts
+++ gourmet-greet-frontend/src/app/recipe-form/recipe-form.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,12 +8,13 @@ import { MatIconModule } from '@angular/material/icon';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../interfaces/category.interface';
-import { forkJoin, map, mergeMap, Observable, of, switchMap } from 'rxjs';
+import { forkJoin, map, merge, mergeMap, Observable, of, switchMap } from 'rxjs';
 import { RecipeAddDto } from '../interfaces/recipe-add.interface';
 import { UserService } from '../user.service';
 import { RecipeService } from '../services/recipe.service';
 import { Recipe } from '../interfaces/recipe.interface';
 import { Router } from '@angular/router';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-recipe-form',
@@ -38,7 +38,11 @@ export class RecipeFormComponent implements OnInit {
   galleryFiles: File[] = [];
   galleryPreviewUrls: string[] = [];
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  titleErrorMessage = signal('');
+  ingredientsErrorMessage = signal('');
+  stepsErrorMessage = signal('');
+
+  constructor(private fb: FormBuilder) {
     this.recipeForm = this.fb.group({
       title: ['', Validators.required],
       description: [''],
@@ -48,6 +52,75 @@ export class RecipeFormComponent implements OnInit {
       cookingTime: [0],
       servings: [0]
     });
+
+    const titleControl = this.recipeForm.get('title') as FormControl;
+    merge(titleControl?.statusChanges, titleControl?.valueChanges)
+    .pipe(takeUntilDestroyed())
+    .subscribe(() => this.updateTitleErrorMessage())
+
+    this.ingredients.valueChanges.subscribe(() => {
+      this.ingredientErrorMessage();
+    });
+  
+    this.ingredients.statusChanges.subscribe(() => {
+      this.ingredientErrorMessage();
+    });
+
+    this.steps.valueChanges.subscribe(() => {
+      this.stepErrorMessage();
+    });
+  
+    this.steps.statusChanges.subscribe(() => {
+      this.stepErrorMessage();
+    });
+  }
+
+  ingredientErrorMessage(): string {
+    if (this.ingredients.length === 0) return '';
+    let errorMessages: string[] = [];
+    this.ingredients.controls.forEach((control, index) => {
+      if (control.invalid) {
+        errorMessages.push('Ingredient is required.');
+      }
+    });
+    return errorMessages.join(' ');
+  }
+
+  stepErrorMessage(): string {
+    if (this.steps.length === 0) return '';
+    let errorMessages: string[] = [];
+    this.steps.controls.forEach((control) => {
+      if (control.invalid) {
+        errorMessages.push('Step is required.');
+      }
+    });
+    return errorMessages.join(' ');
+  }
+
+  updateTitleErrorMessage(){
+    if(this.recipeForm.get('title')?.hasError('required')){
+      this.titleErrorMessage.set('You must enter a value');
+    } else {
+      this.titleErrorMessage.set('');
+    }
+  }
+
+  updateFormErrorMessages(){
+    this.updateTitleErrorMessage()
+
+    if (this.ingredients.length === 0) {
+      this.ingredientsErrorMessage.set('Add at least one ingredient!');
+    } else {
+      this.ingredientsErrorMessage.set('');
+      this.ingredientErrorMessage()
+    }
+  
+    if (this.steps.length === 0) {
+      this.stepsErrorMessage.set('Add at least one step!');
+    } else {
+      this.stepsErrorMessage.set('');
+      this.stepErrorMessage()
+    }
   }
 
   ngOnInit(): void {
@@ -86,7 +159,7 @@ export class RecipeFormComponent implements OnInit {
   }
 
   addIngredient() {
-    this.ingredients.push(new FormControl(''));
+    this.ingredients.push(new FormControl('', Validators.required));
   }
 
   removeIngredient(index: number) {
@@ -94,7 +167,7 @@ export class RecipeFormComponent implements OnInit {
   }
 
   addStep() {
-    this.steps.push(new FormControl(''));
+    this.steps.push(new FormControl('', Validators.required));
   }
 
   removeStep(index: number) {
@@ -110,9 +183,19 @@ export class RecipeFormComponent implements OnInit {
       images: []
     };
 
+    const ingredients = recipeDto.ingredients.filter(ingredient => ingredient.length > 0)
+    const steps = recipeDto.steps.filter(step => step.length > 0)
+
+    if(this.recipeForm.invalid || ingredients.length<1 || steps.length<1){
+      this.updateFormErrorMessages()
+      return
+    }
+
     this.userService.getUserDetails().pipe(
       switchMap(user => {
         recipeDto.ownerId = user.id
+        recipeDto.ingredients = recipeDto.ingredients.filter(ingredient => ingredient.length > 0)
+        recipeDto.steps = recipeDto.steps.filter(step => step.length > 0)
         return this.recipeService.createRecipe(recipeDto)
       }),
       switchMap((addedRecipe: Recipe) => {
